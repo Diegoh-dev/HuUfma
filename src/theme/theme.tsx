63e51cfe6aa7ef6b2dc222f6eab602b1027c450b
Diego ribeiro <Diegoh.developer@gmail.com>
2024-04-28 19:05:40 -0300

    feat: Inicando criação do tema do app.

---
+++ src/theme/theme.tsx
@@ -0,0 +1,40 @@
+import {createTheme} from '@shopify/restyle';
+
+export const palette = {
+  purplePrimary: '#414396',
+  purplePrimaryLight: '#DEDEEB',
+  grayBlack: '#000000',
+  grayWhite: '#FFFFFF',
+};
+
+export const theme = createTheme({
+  colors: {
+    ...palette,
+    primary: palette.purplePrimary,
+    primaryContrast: palette.grayWhite,
+
+    background: palette.grayWhite,
+    backgroundContrast: palette.grayBlack,
+
+    //paleta de cores,
+  },
+  spacing: {
+    s4: 4,
+    s8: 8,
+    s10: 10,
+    s12: 12,
+    s14: 14,
+    s16: 16,
+    s20: 20,
+    s24: 24,
+    s32: 32,
+    s40: 40,
+    s48: 48,
+    s56: 56,
+  },
+  borderRadii: {
+    s8: 8,
+    s12: 12,
+    s16: 16,
+  },
+});
