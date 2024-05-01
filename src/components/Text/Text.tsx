81adb6b32399c98e1f0316fb1bfbfb61c2acc2f4
Diego ribeiro <Diegoh.developer@gmail.com>
2024-05-01 17:25:13 -0300

    feat: colocando a font de app vinculada ao componente de text.

--- src/components/Text/Text.tsx
+++ src/components/Text/Text.tsx
@@ -1,21 +1,67 @@
+import React, {ComponentProps, ReactNode} from 'react';
 import {createText} from '@shopify/restyle';
 import {Theme} from '@theme';
-import React, {ComponentProps, ReactNode} from 'react';
-import { TextStyle } from 'react-native';
+import {TextStyle} from 'react-native';
 
 const RNText = createText<Theme>();
 
-type TextProps = ComponentProps<typeof RNText>
+type TextProps = ComponentProps<typeof RNText>;
 
-export interface Props extends TextProps  {
+export interface Props extends TextProps {
   children: ReactNode;
-  preset?:TextVariants;
+  preset?: TextVariants;
+  bold?: boolean;
+  italic?: boolean;
+  semiBold?: boolean;
 }
 
-export function Text({children,preset = 'paragraphMedium', style,...RNTextProps}: Props) {
-
+export function Text({
+  children,
+  preset = 'headingMedium',
+  bold,
+  italic,
+  semiBold,
+  style,
+  ...RNTextProps
+}: Props) {
   const typeSizeFont = $fontSizes[preset];
-  return <RNText style={[typeSizeFont,style]} {...RNTextProps}>{children}</RNText>;
+
+  const fontFamily = getFontFamily(preset, bold, italic, semiBold);
+  return (
+    <RNText style={[typeSizeFont, {fontFamily}, style]} {...RNTextProps}>
+      {children}
+    </RNText>
+  );
+}
+
+function getFontFamily(
+  preset: TextVariants,
+  bold?: boolean,
+  italic?: boolean,
+  semiBold?: boolean,
+) {
+  if (
+    preset === 'headingLarge' ||
+    preset === 'headingMedium' ||
+    preset === 'headingSmall'
+  ) {
+    return italic ? $fontFamily.boldItalic : $fontFamily.bold;
+  }
+
+  switch (true) {
+    case bold && italic:
+      return $fontFamily.boldItalic;
+    case bold:
+      return $fontFamily.bold;
+    case italic:
+      return $fontFamily.italic;
+    case semiBold && italic:
+      return $fontFamily.mediumItalic;
+    case semiBold:
+      return $fontFamily.medium;
+    default:
+      return $fontFamily.regular;
+  }
 }
 
 type TextVariants =
@@ -28,17 +74,30 @@ type TextVariants =
   | 'paragraphCaption'
   | 'paragraphCaptionSmall';
 
-
 // expecificado no prototipo
-  export const $fontSizes : Record<TextVariants,TextStyle> = {
-    headingLarge: {fontSize: 32, lineHeight: 38.4},
-    headingMedium: {fontSize: 22, lineHeight: 26.4},
-    headingSmall: {fontSize: 18, lineHeight: 23.4},
+export const $fontSizes: Record<TextVariants, TextStyle> = {
+  headingLarge: {fontSize: 32, lineHeight: 38.4},
+  headingMedium: {fontSize: 22, lineHeight: 26.4},
+  headingSmall: {fontSize: 18, lineHeight: 23.4},
 
-    paragraphLarge: {fontSize: 18, lineHeight: 25.2},
-    paragraphMedium: {fontSize: 16, lineHeight: 22.4},
-    paragraphSmall: {fontSize: 14, lineHeight: 19.6},
+  paragraphLarge: {fontSize: 18, lineHeight: 25.2},
+  paragraphMedium: {fontSize: 16, lineHeight: 22.4},
+  paragraphSmall: {fontSize: 14, lineHeight: 19.6},
 
-    paragraphCaption: {fontSize: 12, lineHeight: 16.8},
-    paragraphCaptionSmall: {fontSize: 10, lineHeight: 14},
+  paragraphCaption: {fontSize: 12, lineHeight: 16.8},
+  paragraphCaptionSmall: {fontSize: 10, lineHeight: 14},
+};
+
+// fontes do app (devem receber o msm nome que esta no arquivo de fontes)
+export const $fontFamily = {
+  black: 'Satoshi-Black',
+  blackItalic: 'Satoshi-BlackItalic',
+  bold: 'Satoshi-Bold',
+  boldItalic: 'Satoshi-BoldItalic',
+  italic: 'Satoshi-Italic',
+  light: 'Satoshi-Light',
+  lightItalic: 'Satoshi-LightItalic',
+  medium: 'Satoshi-Medium',
+  mediumItalic: 'Satoshi-MediumItalic',
+  regular: 'Satoshi-Regular',
 };
