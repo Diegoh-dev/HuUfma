189389f410dfd489e0c0385d40c65f01c10f82d9
Diego ribeiro <Diegoh.developer@gmail.com>
2024-05-16 00:13:40 -0300

    feat: Componente de icon

---
+++ src/components/Icon/icon.tsx
@@ -0,0 +1,42 @@
+import {useAppTheme} from '@hooks';
+import {ThemeColor} from '@theme';
+import {Pressable} from 'react-native';
+import { ArrowLeftIcon } from '../../assets/icons/ArrowLeftIcon';
+
+
+export interface IconBase {
+  size: number;
+  color: string;
+}
+
+export interface IconPros {
+  name: IconNames;
+  size?: number;
+  color?: ThemeColor;
+  onPress?: () => void;
+}
+export function Icon({
+  name,
+  color = 'backgroundContrast',
+  size = 20,
+  onPress,
+}: IconPros) {
+  const SVGIcon = IconResgistry[name];
+  const {colors} = useAppTheme();
+  if (onPress) {
+    return (
+      <Pressable onPress={onPress} hitSlop={20}>
+        <SVGIcon color={colors[color]} size={size} />
+      </Pressable>
+    );
+  }
+
+  return <SVGIcon color={colors[color]} size={size} />;
+}
+
+const IconResgistry = {
+  arrowLeft: ArrowLeftIcon,
+};
+
+export type IconTypes = typeof IconResgistry;
+export type IconNames = keyof IconTypes;
