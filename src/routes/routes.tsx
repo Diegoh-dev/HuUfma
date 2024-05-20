6cbdeaab485b049bf3d4b850a2690953e3d4f24a
Diego ribeiro <Diegoh.developer@gmail.com>
2024-05-20 13:26:09 -0300

    feat: Rota de Loginscreen;

--- src/routes/routes.tsx
+++ src/routes/routes.tsx
@@ -1,9 +1,10 @@
 import {NavigationContainer} from '@react-navigation/native';
 import {createNativeStackNavigator} from '@react-navigation/native-stack';
-import { SignUpScreen } from '@screens';
+import { LoginScreen, SignUpScreen } from '@screens';
 
 type RootStackParamList = {
     SignUpScreen:undefined;
+    LoginScreen:undefined;
 }
 
 const Stack = createNativeStackNavigator<RootStackParamList>();
@@ -12,10 +13,11 @@ const Stack = createNativeStackNavigator<RootStackParamList>();
 export function Router() {
   return (
     <NavigationContainer >
-      <Stack.Navigator initialRouteName='SignUpScreen' screenOptions={{
+      <Stack.Navigator initialRouteName='LoginScreen' screenOptions={{
         headerShown:false,
         fullScreenGestureEnabled:true,// para aumentar a area arastavel nos dipositivos IOS
       }}>
+        <Stack.Screen name='LoginScreen' component={LoginScreen}/>
         <Stack.Screen name='SignUpScreen' component={SignUpScreen}/>
       </Stack.Navigator>
     </NavigationContainer>
