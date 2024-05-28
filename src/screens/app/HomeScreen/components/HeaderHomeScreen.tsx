b592e66df29fb3337bd5f208b52a28e65f2d5644
Diego ribeiro <Diegoh.developer@gmail.com>
2024-05-28 13:59:19 -0300

    feat: novos icones do app.

---
+++ src/screens/app/HomeScreen/components/HeaderHomeScreen.tsx
@@ -0,0 +1,46 @@
+import {Box, Icon, Text} from '@components';
+import {useAppTheme} from '@hooks';
+import React from 'react';
+import {StatusBar} from 'react-native';
+
+export function HeaderHomeScreen() {
+  const {colors} = useAppTheme();
+  return (
+    <>
+      <StatusBar
+        animated={true}
+        backgroundColor={colors['primary']}
+        showHideTransition={'fade'}
+        networkActivityIndicatorVisible={true}
+      />
+      <Box
+        flexDirection="row"
+        alignItems="center"
+        justifyContent="space-between"
+        backgroundColor="primary"
+        padding="s14"
+        height={100}>
+        <Box flexDirection="row" gap="s12" alignItems="center">
+          <Box
+            backgroundColor="grayWhite"
+            width={50}
+            height={50}
+            borderRadius="s16"></Box>
+
+          <Box>
+            <Text preset="paragraphMedium" color="grayWhite" bold>
+              Olá, Diego
+            </Text>
+            <Text preset="paragraphSmall" color="grayWhite" bold>
+              Prontuário: 14438232
+            </Text>
+          </Box>
+        </Box>
+
+        <Box marginLeft="s4">
+          <Icon name="bellIcon" color="grayWhite" />
+        </Box>
+      </Box>
+    </>
+  );
+}
