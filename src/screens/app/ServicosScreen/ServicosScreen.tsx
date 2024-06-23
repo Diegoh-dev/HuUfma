14811bb9ace0f3a7ae6e56df8ba2a17fcf5afbe2
Diego ribeiro <Diegoh.developer@gmail.com>
2024-06-23 20:31:41 -0300

    feat: novos botões de serviços;

--- src/screens/app/ServicosScreen/ServicosScreen.tsx
+++ src/screens/app/ServicosScreen/ServicosScreen.tsx
@@ -85,7 +85,111 @@ export function ServicosScreen({
                 <Icon name="homeFill" color="grayWhite" size={30} />
               </Box>
               <Text preset="headingMedium" bold>
-                Outros
+              Vacinação
+              </Text>
+            </Box>
+          </Box>
+        </Box>
+        <Box borderRadius="s16" mt="s12">
+          <Box flexDirection="row" borderRadius="s16" style={$shadowProps}>
+            <Box
+              width={7}
+              borderTopLeftRadius="s8"
+              borderBottomLeftRadius="s8"
+              height={'auto'}
+              backgroundColor="primary"></Box>
+            <Box
+              flex={2}
+              alignItems="center"
+              flexDirection="row"
+              backgroundColor="gray5"
+              borderTopRightRadius="s8"
+              borderBottomRightRadius="s8"
+              padding="s20"
+              gap="s10">
+              <Box backgroundColor="primary" padding="s8" borderRadius="s8">
+                <Icon name="science" color="grayWhite" size={30} />
+              </Box>
+              <Text preset="headingMedium" bold>
+              Laboratório
+              </Text>
+            </Box>
+          </Box>
+        </Box>
+        <Box borderRadius="s16" mt="s12">
+          <Box flexDirection="row" borderRadius="s16" style={$shadowProps}>
+            <Box
+              width={7}
+              borderTopLeftRadius="s8"
+              borderBottomLeftRadius="s8"
+              height={'auto'}
+              backgroundColor="primary"></Box>
+            <Box
+              flex={2}
+              alignItems="center"
+              flexDirection="row"
+              backgroundColor="gray5"
+              borderTopRightRadius="s8"
+              borderBottomRightRadius="s8"
+              padding="s20"
+              gap="s10">
+              <Box backgroundColor="primary" padding="s8" borderRadius="s8">
+                <Icon name="homeFill" color="grayWhite" size={30} />
+              </Box>
+              <Text preset="headingMedium" bold>
+              Telemedicina
+              </Text>
+            </Box>
+          </Box>
+        </Box>
+        <Box borderRadius="s16" mt="s12">
+          <Box flexDirection="row" borderRadius="s16" style={$shadowProps}>
+            <Box
+              width={7}
+              borderTopLeftRadius="s8"
+              borderBottomLeftRadius="s8"
+              height={'auto'}
+              backgroundColor="primary"></Box>
+            <Box
+              flex={2}
+              alignItems="center"
+              flexDirection="row"
+              backgroundColor="gray5"
+              borderTopRightRadius="s8"
+              borderBottomRightRadius="s8"
+              padding="s20"
+              gap="s10">
+              <Box backgroundColor="primary" padding="s8" borderRadius="s8">
+                <Icon name="homeFill" color="grayWhite" size={30} />
+              </Box>
+              <Text preset="headingMedium" bold>
+              Fisioterapia e Reabilitação
+              </Text>
+            </Box>
+          </Box>
+        </Box>
+        <Box borderRadius="s16" mt="s12">
+          <Box flexDirection="row" borderRadius="s16" style={$shadowProps}>
+            <Box
+              width={7}
+              borderTopLeftRadius="s8"
+              borderBottomLeftRadius="s8"
+              height={'auto'}
+              backgroundColor="primary"></Box>
+            <Box
+              flex={2}
+              alignItems="center"
+              flexDirection="row"
+              backgroundColor="gray5"
+              borderTopRightRadius="s8"
+              borderBottomRightRadius="s8"
+              padding="s20"
+              gap="s10">
+              <Box backgroundColor="primary" padding="s8" borderRadius="s8">
+                <Icon name="homeFill" color="grayWhite" size={30} />
+              </Box>
+              <Text preset="headingMedium" bold>
+              Cuidados Domiciliares
               </Text>
             </Box>
           </Box>
