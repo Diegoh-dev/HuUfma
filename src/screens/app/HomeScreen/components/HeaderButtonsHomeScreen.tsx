0237b05b2166c102eeb764ebe772992b76a08e3b
Diego ribeiro <Diegoh.developer@gmail.com>
2024-08-06 23:32:05 -0300

    removendo titulos dos botões superiores.

--- src/screens/app/HomeScreen/components/HeaderButtonsHomeScreen.tsx
+++ src/screens/app/HomeScreen/components/HeaderButtonsHomeScreen.tsx
@@ -11,16 +11,17 @@ export function HeaderButtonsHomeScreen() {
      <Text fontWeight='bold' color='teste' mt="s16" mb="s16" preset="headingMedium">Serviços</Text>
      <Text color='teste' mt="s16" mb="s16" preset="headingSmall">Mostrar todos</Text>
      </Box> */}
-      <Text
+      {/* <Text
         fontWeight="bold"
         color="primary"
         mt="s10"
         mb="s10"
         preset="headingMedium">
         Serviços
-      </Text>
+      </Text> */}
       <Box
         // padding="s12"
+        mt="s16"
         flexDirection="row"
         justifyContent="space-around"
         gap='s10'
